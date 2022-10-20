6183a96c2be3c9ec35a43f85970554f94d57a390
anla <1o1ui@anladeiMac.local>
2022-10-20 19:05:18 +0800

    搭建框架

---
+++ packages/ui/src/components/button/button.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+import Taro from "@tarojs/taro";
+import { View, Button } from "@tarojs/components";
+
+const ButtonComponent: FC = () => {
+  return <Button>我是按钮</Button>;
+};
+
+export default ButtonComponent;
