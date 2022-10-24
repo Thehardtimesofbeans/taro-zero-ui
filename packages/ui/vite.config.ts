5439aaf560dd318eebdf25f03fe895835cc002e7
anla <1o1ui@anladeiMac.local>
2022-10-24 19:01:50 +0800

    添加打包库

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,16 +1,31 @@
 import { defineConfig } from "vite";
 
-export default defineConfig({
-    // 添加库模式配置
+const externalPackages = [
+    'react',
+    'react-dom',
+    '@tarojs/react',
+    '@tarojs/taro',
+    '@tarojs/components',
+    '@tarojs/runtime',
+]
 
+export default defineConfig({
+    plugins: [],
+    // 添加库模式配置
     build: {
-        minify: `terser`, // boolean | 'terser' | 'esbuild'
+        rollupOptions: {
+            external: externalPackages,
+            output: {
+
+            }
+        },
+        minify: `esbuild`, // boolean | 'terser' | 'esbuild'
         sourcemap: true, // 输出单独 source文件
         brotliSize: true, // 生成压缩大小报告
         lib: {
             entry: "./src/index.ts",
-            name: "demoui",
-            fileName: "demoui",
+            name: "zeroui",
+            fileName: "zeroui",
             formats: ["esm", "umd", "iife"], // 导出模块类型
         },
         outDir: "./dist",
