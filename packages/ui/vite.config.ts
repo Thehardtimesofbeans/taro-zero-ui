9e942fd88215ab557cbd198c5d9fe2ddd006a52a
anla <1o1ui@anladeiMac.local>
2022-10-26 11:29:36 +0800

    config配置更改

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,4 +1,7 @@
 import { defineConfig } from "vite";
+import path from 'path'
+import config from './package.json'
+
 
 const externalPackages = [
     'react',
@@ -13,20 +16,27 @@ export default defineConfig({
     plugins: [],
     // 添加库模式配置
     build: {
+        minify: false, // boolean | 'terser' | 'esbuild'
         rollupOptions: {
+            // 请确保外部化那些你的库中
             external: externalPackages, // 将以上模块保留在bundle之外,不让以上插件打包到组件库中
             output: {
-
+                // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+                globals: {
+                    react: 'React',
+                    'react-dom': 'ReactDOM'
+                },
+                plugins: [],
             }
         },
-        minify: `esbuild`, // boolean | 'terser' | 'esbuild'
+
         sourcemap: true, // 输出单独 source文件
-        brotliSize: true, // 生成压缩大小报告
+
         lib: {
             entry: "./src/index.ts",
             name: "zeroui",
             fileName: "zeroui",
-            formats: ["esm", "umd", "iife"], // 导出模块类型
+            formats: ["es", "umd", "iife"], // 导出模块类型
         },
         outDir: "./dist",
     },
